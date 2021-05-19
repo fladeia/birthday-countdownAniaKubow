4fb1e9cf86f237482e381c94d295aa8b806268c0
fladeia <252842.5s@gmail.com>
2021-05-19 13:48:05 -0300

    birthday countdown

---
+++ app.js
@@ -0,0 +1,37 @@
+const timeLeft = document.querySelector('time-left')
+
+const today = new Date()
+const birthday = new Date('11/04/2022')
+// milliseconds
+const second = 1000
+const minute = second * 60
+const hour = minute * 60
+const day = hour * 24
+let timerId
+
+console.log(today)
+console.log(birthday)
+
+function countdown() {
+    const today = new Date()
+    const timeSpan = birthday - today
+
+    if(timeSpan <= -day) {
+        timeLeft.innerHTML = "Hope you had a nice Birthday!"
+        clearInterval(timerId)
+        return
+    }
+    if(timeSpan <= 0) {
+        timeLeft.innerHTML = "Happy Birthday"
+        clearInterval(timerId)
+        return
+    }
+
+    const days = Math.floor(timeSpan / day)
+    const hours = Math.floor((timeSpan % day) / hour)
+    const minutes = Math.floor((timeSpan % hour) / minute)
+    const seconds = Math.floor((timeSpan % minute) / second)
+
+    timeLeft.innerHTML = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`
+}
+setInterval(countdown, second)
---
+++ note.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/watch?v=V-Mcul5kS_Y&ab_channel=CodewithAniaKub%C3%B3w
+
+Build a Birthday Countdown in JavaScript (super simple!)
